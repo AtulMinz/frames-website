e5790216bc63404d4af76c57a484f5074be0191e
AtulMinz <atulminz.jsr@gmail.com>
2025-03-24 13:20:16 +0000

    Added Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Hero from "@/pages/Hero";
+import Footer from "@/pages/Footer";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Footer />
+    </>
+  );
 }
---
+++ src/pages/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="">
+        <p className="text-xs font-semibold font-sans">
+          Made with ❤️ by Atul Minz
+        </p>
+      </div>
+    </footer>
+  );
+}
