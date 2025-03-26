1afa2e5c060be1b9ff7906bea38063d15a30de8d
AtulMinz <atulminz.jsr@gmail.com>
2025-03-26 09:35:55 +0000

    Added data file

---
+++ src/lib/mockData.ts
@@ -0,0 +1,102 @@
+export const data = [
+  {
+    title:
+      "decentralized community notes using token rewards for submitting accurate info",
+    status: "in progress",
+  },
+  {
+    title:
+      "Pooltogether frame - easily depositing your USDC Rewards to onchain prize savings",
+    status: "in progress",
+  },
+  {
+    title: "turning any live stream url into a live chat for Farcaster users",
+    status: "in progress",
+  },
+  { title: "crypto mahjong", status: "in progress" },
+  { title: "FarMart similar to Amazon/tiktok shop", status: "in progress" },
+  {
+    title:
+      "A high school style yearbook but for farcaster. PFP gallery of all users + give me a place to sign 'have a great summer'",
+    status: "in progress",
+  },
+  { title: "Group buy mini-app", status: "in progress" },
+  { title: "Tinder onchain", status: "in progress" },
+  { title: "'pop up' waifu frame with minting", status: "in progress" },
+  {
+    title:
+      "Index NFT mints, candles for sale, donations, other onchain tchotchkes",
+    status: "in progress",
+  },
+  {
+    title: "Send a Valentine to another Farcaster user",
+    status: "in progress",
+  },
+  {
+    title: "SOL-based coin airdrop for FC accounts with SOL-connected wallets",
+    status: "in progress",
+  },
+  {
+    title:
+      "Sims-like relationship frame/app for measuring your standing with recently interacted people",
+    status: "in progress",
+  },
+  {
+    title:
+      "frame that allows you to post something and lock the content behind the paywall or make it available only to some fids",
+    status: "in progress",
+  },
+  { title: "chatroulette for farcaster builders", status: "in progress" },
+  { title: "Send voice messages", status: "in progress" },
+  {
+    title:
+      "a reverse bounty board where moonshot ideas requesting funding are posted like grant requests",
+    status: "in progress",
+  },
+  {
+    title: "disperse.app as a Base Frame w/ social graph",
+    status: "in progress",
+  },
+  { title: "Chinese New Year frame", status: "in progress" },
+  {
+    title:
+      "Governance frame: Use the votes surfaced by Agora, identify if you're eligible for a vote, send in-app notification",
+    status: "in progress",
+  },
+  { title: "Charity of the week frame", status: "in progress" },
+  { title: "Onchain Powerball", status: "in progress" },
+  { title: "Bingo", status: "in progress" },
+  { title: "Poker", status: "in progress" },
+  {
+    title: "Onchain time-based / turn-based social game",
+    status: "in progress",
+  },
+  { title: "Notifies you if your frame expires soon", status: "in progress" },
+  { title: "Guided meditations", status: "in progress" },
+  { title: "Space Trader or Drug Wars game", status: "in progress" },
+  { title: "Freeciv Longturn Farcaster", status: "in progress" },
+  { title: "Crypto charity donations", status: "in progress" },
+  { title: "Farcaster fishing minigame", status: "in progress" },
+  {
+    title: "linktree of all the FC native token launchers",
+    status: "in progress",
+  },
+  { title: "GoFundMe for Farcaster", status: "in progress" },
+  {
+    title: "A Turing Test app - join a group chat and guess who the bot is",
+    status: "in progress",
+  },
+  { title: "Fario Party multiplayer inframe minigames", status: "in progress" },
+  {
+    title:
+      "Skribbl - group drawing game where you guess a drawing from someone else",
+    status: "in progress",
+  },
+  { title: "Foursquare 2.0", status: "in progress" },
+  { title: "Sell a file frame / cryptonative gumroad", status: "in progress" },
+  {
+    title:
+      "Carousel frame - tap open the frame, display a grid of images like an Instagram profile",
+    status: "in progress",
+  },
+];
