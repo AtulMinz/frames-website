1b2f39dd9c6091c83c8198e481c5814452ad8409
AtulMinz <atulminz.jsr@gmail.com>
2025-03-26 09:36:01 +0000

    Updated Footer

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      <div className="">
+      <div className="flex justify-end w-[95vw] mt-12">
         <p className="text-xs font-semibold font-sans">
           Made with ❤️ by Atul Minz
         </p>
